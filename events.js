8bfa6dcdd21ece49a6119b6525c353a60ae38b31
Utkarsh Dubey <139694888+utkarshh7@users.noreply.github.com>
2024-12-20 12:16:47 +0530

    .

---
+++ events.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('nav');
+
+document.addEventListener('scroll', (e)=>{
+    if(window.scrollY > 120){
+       nav.style.background = '#171717' 
+    }
+    else{
+        nav.style.background = '' 
+    }
+})
